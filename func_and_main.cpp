a8aa5953deff54299ff864259625b0b251b29ffd
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2022-02-11 09:19:43 +0300

    Update func_and_main.cpp

--- func_and_main.cpp
+++ func_and_main.cpp
@@ -5,44 +5,44 @@
 
 using namespace std;
 
-void itc_horizontalLine(SDL_Renderer *renderer, int x, int y, int len){    
+void itc_horizontalLine(SDL_Renderer* renderer, int x, int y, int len) {
 
     int n;
-    if(len != 0){
-        for(n = 0; n < len; n++){
-            SDL_RenderDrawPoint(renderer, x + n , y );
+    if (len != 0) {
+        for (n = 0; n < len; n++) {
+            SDL_RenderDrawPoint(renderer, x + n, y);
         }
     }
 }
 
-void itc_verticalLine(SDL_Renderer *renderer, int x, int y, int len){
+void itc_verticalLine(SDL_Renderer* renderer, int x, int y, int len) {
     int n;
-    if(len != 0){
-        for(n = 0; n < len; n++){
-            SDL_RenderDrawPoint(renderer, x, y + n );
+    if (len != 0) {
+        for (n = 0; n < len; n++) {
+            SDL_RenderDrawPoint(renderer, x, y + n);
         }
     }
 }
 
-void itc_verticalDiagram(SDL_Renderer *render, vector<int>num){
+void itc_verticalDiagram(SDL_Renderer* render, vector<int>num) {
 
-    for (int i = 0; i < num.size(); i++){
-        itc_verticalLine(render, 10 + 100 * i, 1000, numbers[i]);
+    for (int i = 0; i < num.size(); i++) {
+        itc_verticalLine(render, 10 + 100 * i, 1000, num[i]);
     }
 
 }
 
-void itc_horizontalDiagram(SDL_Renderer *render, vector<int>num){
-    for (int i = 0; i < numbers.size(); i++){
-        itc_horizontalLine(render, 1000, 100 + 10 * i, numbers[i]);
+void itc_horizontalDiagram(SDL_Renderer* render, vector<int>num) {
+    for (int i = 0; i < num.size(); i++) {
+        itc_horizontalLine(render, 1000, 100 + 10 * i, num[i]);
     }
 }
 
-int main( int argc, char* args[] )
+int main(int argc, char* args[])
 {
-    SDL_Renderer *renderer;
-    SDL_Window *window;
-    SDL_Init( SDL_INIT_EVERYTHING);
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_CreateWindowAndRenderer(WINDOW_SIZE, WINDOW_SIZE, 0, &window, &renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
